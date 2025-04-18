ff04c648c7f2589039a00567394acff6a4270f59
Amepistheo <kellylhji@gmail.com>
2025-04-18 18:15:24 +0900

    [REFACTOR] #17: 공통 질문 엔티티 및 주문서 질문 엔티티 생성

---
+++ src/main/java/com/deark/be/order/domain/OrderQuestion.java
@@ -0,0 +1,47 @@
+package com.deark.be.order.domain;
+
+import com.deark.be.order.domain.type.QuestionType;
+import com.deark.be.store.domain.Store;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "order_question")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class OrderQuestion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_question_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "common_question_id")
+    private CommonQuestion commonQuestion;
+
+    @Column(name = "question_type", nullable = false)
+    private QuestionType questionType;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "is_required", nullable = false)
+    private Boolean isRequired;
+
+    @Builder
+    public OrderQuestion(Store store, CommonQuestion commonQuestion, QuestionType questionType, String content, Boolean isRequired) {
+        this.store = store;
+        this.commonQuestion = commonQuestion;
+        this.questionType = questionType;
+        this.content = content;
+        this.isRequired = isRequired;
+    }
+}
