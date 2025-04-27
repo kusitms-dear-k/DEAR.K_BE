41c373bd837a1978dc2cb87d9920ae5b32ae4efa
Amepistheo <kellylhji@gmail.com>
2025-04-27 20:53:27 +0900

    [REFACTOR] #20: 사장님 더미 데이터 추가

--- src/main/java/com/deark/be/user/repository/init/UserInitializer.java
+++ src/main/java/com/deark/be/user/repository/init/UserInitializer.java
@@ -29,6 +29,7 @@ public class UserInitializer implements ApplicationRunner {
         } else {
             List<User> userList = new ArrayList<>();
 
+            // 기존 관리자, 고객, 사장님 1명
             User DUMMY_ADMIN = User.builder()
                     .name("관리자")
                     .email("admin@email.com")
@@ -47,7 +48,7 @@ public class UserInitializer implements ApplicationRunner {
                     .isBlacklist(false)
                     .build();
 
-            User DUMMY_USER2 = User.builder()
+            User DUMMY_OWNER0 = User.builder()
                     .name("사장이")
                     .email("owner@email.com")
                     .phone("010-8459-1256")
@@ -56,8 +57,58 @@ public class UserInitializer implements ApplicationRunner {
                     .isBlacklist(false)
                     .build();
 
+            User DUMMY_OWNER1 = User.builder()
+                    .name("김대표")
+                    .email("kim.owner1@email.com")
+                    .phone("010-2345-6781")
+                    .socialId("owner1SocialId")
+                    .role(OWNER)
+                    .isBlacklist(false)
+                    .build();
+
+            User DUMMY_OWNER2 = User.builder()
+                    .name("이대표")
+                    .email("lee.owner2@email.com")
+                    .phone("010-3456-7892")
+                    .socialId("owner2SocialId")
+                    .role(OWNER)
+                    .isBlacklist(false)
+                    .build();
+
+            User DUMMY_OWNER3 = User.builder()
+                    .name("박대표")
+                    .email("park.owner3@email.com")
+                    .phone("010-4567-8912")
+                    .socialId("owner3SocialId")
+                    .role(OWNER)
+                    .isBlacklist(false)
+                    .build();
+
+            User DUMMY_OWNER4 = User.builder()
+                    .name("최대표")
+                    .email("choi.owner4@email.com")
+                    .phone("010-5678-9123")
+                    .socialId("owner4SocialId")
+                    .role(OWNER)
+                    .isBlacklist(false)
+                    .build();
+
+            User DUMMY_OWNER5 = User.builder()
+                    .name("정대표")
+                    .email("jung.owner5@email.com")
+                    .phone("010-6789-1234")
+                    .socialId("owner5SocialId")
+                    .role(OWNER)
+                    .isBlacklist(false)
+                    .build();
+
             userList.add(DUMMY_USER1);
-            userList.add(DUMMY_USER2);
+            userList.add(DUMMY_OWNER0);
+            userList.add(DUMMY_OWNER1);
+            userList.add(DUMMY_OWNER2);
+            userList.add(DUMMY_OWNER3);
+            userList.add(DUMMY_OWNER4);
+            userList.add(DUMMY_OWNER5);
             userList.add(DUMMY_ADMIN);
 
             userRepository.saveAll(userList);
