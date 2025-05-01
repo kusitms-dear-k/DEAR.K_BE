67237a35e592e0ffb784228bc5e5bd6f072c9a8a
JeongIna <jeongina89@gmail.com>
2025-05-01 20:07:56 +0900

    [FEAT]:가게 입점 등록 + 기본 정보 등록 기능 구현

--- src/main/java/com/deark/be/store/controller/StoreController.java
+++ src/main/java/com/deark/be/store/controller/StoreController.java
@@ -1,9 +1,25 @@
 package com.deark.be.store.controller;
 
+import static com.deark.be.global.dto.ResponseTemplate.EMPTY_RESPONSE;
+
+import com.deark.be.global.dto.ResponseTemplate;
+import com.deark.be.store.dto.request.StoreBasicInfoRequest;
+import com.deark.be.store.dto.request.StoreRegisterRequest;
+import com.deark.be.store.service.StoreService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Store", description = "가게 관련 API")
@@ -12,4 +28,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/store")
 public class StoreController {
+    private final StoreService storeService;
+
+    @PostMapping(value = "/register", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "가게 입점 등록", description = """
+        S3에 미리 업로드된 파일 URL을 기반으로 가게 입점 정보를 등록합니다.
+        """)
+    public ResponseEntity<ResponseTemplate<Object>> registerStore(
+            @ModelAttribute @Valid StoreRegisterRequest request,
+            @Parameter(description = "사용자 ID", example = "1")
+            @RequestParam Long userId
+    ) {
+        Long storeId = storeService.registerStore(request, userId);
+        return ResponseEntity
+                .ok(ResponseTemplate.from(storeId));
+    }
+
+    @PostMapping("/basic-info")
+    @Operation(summary = "가게 기본 정보 등록", description = "입점 후, 기본정보 및 영업시간을 등록합니다.")
+    public ResponseEntity<ResponseTemplate<Object>> registerStoreBasicInfo(
+            @RequestParam Long storeId,
+            @RequestBody StoreBasicInfoRequest request
+    ) {
+        storeService.registerStoreBasicInfo(storeId, request);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(EMPTY_RESPONSE);
+    }
 }
--- src/main/java/com/deark/be/store/service/StoreService.java
+++ src/main/java/com/deark/be/store/service/StoreService.java
@@ -1,5 +1,17 @@
 package com.deark.be.store.service;
 
+import static com.deark.be.store.exception.errorcode.StoreErrorCode.STORE_NOT_FOUND;
+import static com.deark.be.user.exception.errorcode.UserErrorCode.USER_NOT_FOUND;
+
+import com.deark.be.global.service.S3Service;
+import com.deark.be.store.domain.Store;
+import com.deark.be.store.dto.request.StoreBasicInfoRequest;
+import com.deark.be.store.dto.request.StoreRegisterRequest;
+import com.deark.be.store.exception.StoreException;
+import com.deark.be.store.repository.StoreRepository;
+import com.deark.be.user.domain.User;
+import com.deark.be.user.exception.UserException;
+import com.deark.be.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,4 +22,35 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class StoreService {
+    private final StoreRepository storeRepository;
+    private final UserRepository userRepository;
+    private final BusinessHoursService businessHoursService;
+    private final S3Service s3Service;
+
+
+    @Transactional
+    public Long registerStore(StoreRegisterRequest request, Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserException(USER_NOT_FOUND));
+        String businessLicenseUrl = s3Service.uploadFile(request.businessLicenseFile());
+        String businessPermitUrl = s3Service.uploadFile(request.businessPermitFile());
+
+        Store store = request.toEntity(user, businessLicenseUrl, businessPermitUrl);
+        return storeRepository.save(store).getId();
+    }
+
+    @Transactional
+    public void registerStoreBasicInfo(Long storeId, StoreBasicInfoRequest request) {
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(() -> new StoreException(STORE_NOT_FOUND));
+
+
+        store.updateBasicInfo(
+                request.name(), request.phone(), request.description(), request.address(),
+                request.imageUrl(), request.chattingUrl(), request.isUnmanned(),
+                request.orderLink(), request.maxDailyOrders()
+        );
+
+        businessHoursService.registerBusinessHours(store, request.businessHours());
+    }
 }
