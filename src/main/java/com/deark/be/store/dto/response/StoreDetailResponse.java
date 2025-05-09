815bfdfec60e33039bfd8d647a6d17470e0e476f
JeongIna <jeongina89@gmail.com>
2025-05-09 22:58:18 +0900

    [REFACTOR]:StoreDetailResponse 생성 책임을 DTO 내부로 이동

--- src/main/java/com/deark/be/store/dto/response/StoreDetailResponse.java
+++ src/main/java/com/deark/be/store/dto/response/StoreDetailResponse.java
@@ -1,5 +1,6 @@
 package com.deark.be.store.dto.response;
 
+import com.deark.be.store.domain.Store;
 import java.util.List;
 import lombok.Builder;
 
@@ -20,4 +21,30 @@ public record StoreDetailResponse(
         String businessNumber,
         List<String>sizeNameList
 ) {
+    public static StoreDetailResponse from(
+            Store store,
+            boolean is24hSelfService,
+            boolean isLunchBoxCake,
+            List<BusinessHourResponse> businessHours,
+            List<PickUpHourResponse> pickupHours,
+            Long likeCount,
+            List<String> sizeNameList
+    ) {
+        return new StoreDetailResponse(
+                store.getId(),
+                store.getName(),
+                store.getDescription(),
+                store.getImageUrl(),
+                store.getAddress(),
+                store.getIsSameDayOrder(),
+                is24hSelfService,
+                isLunchBoxCake,
+                businessHours,
+                pickupHours,
+                store.getOwnerName(),
+                likeCount,
+                store.getBusinessNumber(),
+                sizeNameList
+        );
+    }
 }
