3b50a28d91e9918a49a8dd9b44e43cbe1d4c7a5b
JeongIna <jeongina89@gmail.com>
2025-05-09 23:47:10 +0900

    [FEAT]: 스토어 상세 조회 서비스 로직 구현

--- src/main/java/com/deark/be/store/service/StoreService.java
+++ src/main/java/com/deark/be/store/service/StoreService.java
@@ -1,12 +1,18 @@
 package com.deark.be.store.service;
 
+import com.deark.be.design.repository.SizeRepository;
+import com.deark.be.event.repository.EventStoreRepository;
 import com.deark.be.global.service.S3Service;
+import com.deark.be.store.domain.BusinessHours;
 import com.deark.be.store.domain.Store;
 import com.deark.be.store.domain.type.SortType;
 import com.deark.be.store.dto.request.StoreBasicInfoRequest;
 import com.deark.be.store.dto.request.StoreRegisterRequest;
+import com.deark.be.store.dto.response.BusinessHourResponse;
+import com.deark.be.store.dto.response.PickUpHourResponse;
 import com.deark.be.store.dto.response.SearchStorePagedResult;
 import com.deark.be.store.dto.response.SearchStoreResponseList;
+import com.deark.be.store.dto.response.StoreDetailResponse;
 import com.deark.be.store.exception.StoreException;
 import com.deark.be.store.repository.StoreRepository;
 import com.deark.be.user.domain.User;
@@ -33,7 +39,9 @@ public class StoreService {
     private final StoreRepository storeRepository;
     private final UserRepository userRepository;
     private final BusinessHoursService businessHoursService;
+    private final SizeRepository sizeRepository;
     private final S3Service s3Service;
+    private final EventStoreRepository eventStoreRepository;
 
     @Transactional
     public Long registerStore(StoreRegisterRequest request, Long userId, MultipartFile businessLicenseFile, MultipartFile businessPermitFile) {
@@ -63,5 +71,38 @@ public class StoreService {
                 keyword, isSameDayOrder, locationList, startDate, endDate, minPrice, maxPrice, isLunchBoxCake);
 
         return SearchStoreResponseList.of(allSearchResult.totalCount(), page, allSearchResult.hasNext(), allSearchResult.storeList());
+    }
+
+    public StoreDetailResponse getstoreDetail(Long storeId) {
+        List <String> sizeNameList=sizeRepository.findSizeNamesByStoreId(storeId);
+        Long likeCount=eventStoreRepository.countByStoreId(storeId);
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(() -> new StoreException(STORE_NOT_FOUND));
+
+        boolean is24hSelfService = Boolean.TRUE.equals(store.getIsSelfService()) &&
+                store.getBusinessHoursList().stream()
+                        .anyMatch(BusinessHours::getIsOpen24Hours);
+
+        boolean isLunchBoxCake = sizeRepository.existsByStoreIdAndNameContaining(storeId, "도시락");
+
+        List<PickUpHourResponse> pickupHours  = store.getBusinessHoursList().stream()
+                .map(PickUpHourResponse::from)
+                .toList();
+
+        List<BusinessHourResponse> businessHours=pickupHours.stream()
+                .map(p->new BusinessHourResponse(p.dayName(),p.startTime(),p.endTime()))
+                .toList();
+
+
+
+        return  StoreDetailResponse.from(
+                store,
+                is24hSelfService,
+                isLunchBoxCake,
+                businessHours,
+                pickupHours,
+                likeCount,
+                sizeNameList
+        );
     }
 }
