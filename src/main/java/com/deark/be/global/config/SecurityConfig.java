1fe4b12afe1a6751b740a3316d4db22edd5f92ab
JeongIna <jeongina89@gmail.com>
2025-05-11 03:07:25 +0900

    [REFACTOR]:CorsConfigurationSource 코드 제거

--- src/main/java/com/deark/be/global/config/SecurityConfig.java
+++ src/main/java/com/deark/be/global/config/SecurityConfig.java
@@ -64,17 +64,4 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
 }
