97ea4e4c407be9f56b24979ae5301dba03ef393c
Ina (PC) <shlovejo2@ajou.ac.kr>
2025-05-15 20:15:03 +0900

    [FEAT]:주문서 질문 & 답변 requestDTO 구현

---
+++ src/main/java/com/deark/be/order/dto/request/QARequest.java
@@ -0,0 +1,22 @@
+package com.deark.be.order.dto.request;
+
+import com.deark.be.order.domain.Message;
+import com.deark.be.order.domain.QA;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "주문서 질문과 답변에 대한 정보")
+public record QARequest(
+        @Schema(description = "질문", example = "이름")
+        String question,
+
+        @Schema(description = "답변", example = "김혜연")
+        String answer
+) {
+    public QA toEntity(Message message) {
+        return QA.builder()
+                .message(message)
+                .question(question)
+                .answer(answer)
+                .build();
+    }
+}
