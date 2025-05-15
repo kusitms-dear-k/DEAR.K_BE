10a0e8000c99632d4a1bb924882d300b13254d67
Amepistheo <kellylhji@gmail.com>
2025-05-15 11:12:48 +0900

    [REFACTOR] #41: 요청서 조회 시 유저 답변을 Map -> List로 제공하도록 변경

--- src/main/java/com/deark/be/order/dto/response/MyOrderStatusResponse.java
+++ src/main/java/com/deark/be/order/dto/response/MyOrderStatusResponse.java
@@ -4,32 +4,36 @@ import com.deark.be.order.domain.Message;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
-import java.time.LocalDate;
-import java.util.Map;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Locale;
 
 @Builder
 public record MyOrderStatusResponse(
         @Schema(description = "쪽지 ID", example = "1")
         Long messageId,
-        @Schema(description = "요청 날짜", example = "2025-05-01")
-        LocalDate createdAt,
+        @Schema(description = "요청 날짜", example = "2025-05-01(목)")
+        String requestDate,
         @Schema(description = "가게 이름", example = "디어케이 케이크")
         String storeName,
         @Schema(description = "디자인 이름", example = "루피 케이크")
         String designName,
         @Schema(description = "디자인 이미지 URL", example = "https://deark.com/design/1/image")
         String designImageUrl,
-        @Schema(description = "주문서 질문 & 답", example = "{ \"크기\": \"도시락 케이크\", \"픽업 희망 날짜\": \"2025년 6월 20일 금요일\", \"픽업 희망 시간\": \"13시 30분\" }")
-        Map<String, String> qaDetails
+        @Schema(description = "주문서 질문 & 답", example = "[{\"title\": \"이름\", \"answer\": \"박지유\"}]")
+        List<QAStatusResponse> qaDetails
 ) {
-    public static MyOrderStatusResponse of(Message message, Map<String, String> qaMap) {
+    public static MyOrderStatusResponse of(Message message, List<QAStatusResponse> qaDetails) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd(E)", Locale.KOREAN);
+        String formattedDate = message.getCreatedAt().format(formatter);
+
         return MyOrderStatusResponse.builder()
                 .messageId(message.getId())
-                .createdAt(message.getCreatedAt().toLocalDate())
+                .requestDate(formattedDate)
                 .storeName(message.getStore().getName())
                 .designName(message.getDesignName())
                 .designImageUrl(message.getDesignImageUrl())
-                .qaDetails(qaMap)
+                .qaDetails(qaDetails)
                 .build();
     }
 }
