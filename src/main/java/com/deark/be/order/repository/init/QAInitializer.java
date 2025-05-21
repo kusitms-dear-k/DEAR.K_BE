6b3c808ed6145b964531c1fd7ddf000b224d155c
Ina (PC) <shlovejo2@ajou.ac.kr>
2025-05-21 11:13:44 +0900

    [FEAT] 기존 더미데이터의 question 값 수정 + MESSAGE5에 대한 QA 더미데이터 추가

--- src/main/java/com/deark/be/order/repository/init/QAInitializer.java
+++ src/main/java/com/deark/be/order/repository/init/QAInitializer.java
@@ -31,6 +31,7 @@ public class QAInitializer implements ApplicationRunner {
             Message MESSAGE1 = messageRepository.findById(1L).orElseThrow();
             Message MESSAGE2 = messageRepository.findById(2L).orElseThrow();
             Message MESSAGE4 = messageRepository.findById(4L).orElseThrow();
+            Message MESSAGE5 = messageRepository.findById(5L).orElseThrow();
             Message MESSAGE7 = messageRepository.findById(7L).orElseThrow();
 
             QA DUMMY_QA1 = QA.builder()
@@ -84,15 +85,15 @@ public class QAInitializer implements ApplicationRunner {
 
             QA DUMMY_QA8 = QA.builder()
                     .message(MESSAGE1)
-                    .question("크림 맛")
-                    .answer("블루베리")
+                    .question("크림")
+                    .answer("블루베리 크림")
                     .isRequired(true)
                     .build();
 
             QA DUMMY_QA9 = QA.builder()
                     .message(MESSAGE1)
-                    .question("시트 맛")
-                    .answer("바닐라")
+                    .question("시트")
+                    .answer("바닐라 시트")
                     .isRequired(true)
                     .build();
 
@@ -147,14 +148,14 @@ public class QAInitializer implements ApplicationRunner {
 
             QA DUMMY_QA17 = QA.builder()
                     .message(MESSAGE2)
-                    .question("크림 맛")
+                    .question("크림")
                     .answer("생크림")
                     .isRequired(true)
                     .build();
 
             QA DUMMY_QA18 = QA.builder()
                     .message(MESSAGE2)
-                    .question("시트 맛")
+                    .question("시트")
                     .answer("초코")
                     .isRequired(true)
                     .build();
@@ -210,14 +211,14 @@ public class QAInitializer implements ApplicationRunner {
 
             QA DUMMY_QA26 = QA.builder()
                     .message(MESSAGE4)
-                    .question("크림 맛")
+                    .question("크림")
                     .answer("딸기")
                     .isRequired(true)
                     .build();
 
             QA DUMMY_QA27 = QA.builder()
                     .message(MESSAGE4)
-                    .question("시트 맛")
+                    .question("시트")
                     .answer("초코")
                     .isRequired(true)
                     .build();
@@ -273,15 +274,78 @@ public class QAInitializer implements ApplicationRunner {
 
             QA DUMMY_QA35 = QA.builder()
                     .message(MESSAGE7)
-                    .question("크림 맛")
+                    .question("크림")
                     .answer("바닐라")
                     .isRequired(true)
                     .build();
 
             QA DUMMY_QA36 = QA.builder()
                     .message(MESSAGE7)
-                    .question("시트 맛")
+                    .question("시트")
                     .answer("바닐라")
+                    .isRequired(true)
+                    .build();
+
+            QA DUMMY_QA37 = QA.builder()
+                    .message(MESSAGE5)
+                    .question("이름")
+                    .answer("정아린")
+                    .isRequired(true)
+                    .build();
+
+            QA DUMMY_QA38 = QA.builder()
+                    .message(MESSAGE5)
+                    .question("전화번호")
+                    .answer("010-5643-4254")
+                    .isRequired(true)
+                    .build();
+
+            QA DUMMY_QA39 = QA.builder()
+                    .message(MESSAGE5)
+                    .question("픽업 희망 일자")
+                    .answer("2025년 6월 20일 금요일")
+                    .isRequired(true)
+                    .build();
+
+            QA DUMMY_QA40 = QA.builder()
+                    .message(MESSAGE5)
+                    .question("픽업 희망 시간")
+                    .answer("21시 00분")
+                    .isRequired(true)
+                    .build();
+
+            QA DUMMY_QA41 = QA.builder()
+                    .message(MESSAGE5)
+                    .question("기타 요청사항")
+                    .answer("없습니다.")
+                    .isRequired(true)
+                    .build();
+
+            QA DUMMY_QA42 = QA.builder()
+                    .message(MESSAGE5)
+                    .question("추가 요청사항")
+                    .answer("배경색은 보라색으로 해주세요")
+                    .isRequired(false)
+                    .build();
+
+            QA DUMMY_QA43 = QA.builder()
+                    .message(MESSAGE5)
+                    .question("크기")
+                    .answer("2호")
+                    .isRequired(true)
+                    .build();
+
+            QA DUMMY_QA44 = QA.builder()
+                    .message(MESSAGE5)
+                    .question("크림")
+                    .answer("바닐라")
+                    .isRequired(true)
+                    .build();
+
+            QA DUMMY_QA45 = QA.builder()
+                    .message(MESSAGE5)
+                    .question("시트")
+                    .answer("초콜릿")
                     .isRequired(true)
                     .build();
 
@@ -290,7 +354,8 @@ public class QAInitializer implements ApplicationRunner {
                     DUMMY_QA10, DUMMY_QA11, DUMMY_QA12, DUMMY_QA13, DUMMY_QA15, DUMMY_QA16, DUMMY_QA17, DUMMY_QA18,
                     DUMMY_QA19, DUMMY_QA20, DUMMY_QA21, DUMMY_QA22, DUMMY_QA24, DUMMY_QA25, DUMMY_QA26, DUMMY_QA27,
                     DUMMY_QA28, DUMMY_QA29, DUMMY_QA30, DUMMY_QA31, DUMMY_QA33, DUMMY_QA34, DUMMY_QA35, DUMMY_QA36,
-                    DUMMY_QA5, DUMMY_QA14, DUMMY_QA23, DUMMY_QA32
+                    DUMMY_QA5, DUMMY_QA14, DUMMY_QA23, DUMMY_QA32,DUMMY_QA37,DUMMY_QA38,DUMMY_QA39,DUMMY_QA40,DUMMY_QA41,
+                    DUMMY_QA42,DUMMY_QA43,DUMMY_QA44,DUMMY_QA45
             ));
 
             qaRepository.saveAll(qaList);
